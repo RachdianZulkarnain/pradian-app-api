90c686f38e16960344d6591279570872b15367a4
RachdianZulkarnain <rachdianmuhammadadha@gmail.com>
2025-07-29 23:03:36 +0700

    update event

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 import { PORT } from "./config/env";
 import { SampleRouter } from "./modules/sample/sample.router";
 import { errorMiddleware } from "./middlewares/error.middleware";
+import { EventRouter } from "./modules/events/event.router";
 
 export class App {
   app: Express;
@@ -20,8 +21,11 @@ export class App {
 
   private routes() {
     const sampleRouter = new SampleRouter();
+    const eventRouter = new EventRouter();
+
 
     this.app.use("/samples", sampleRouter.getRouter());
+    this.app.use("/events", eventRouter.getRouter());
   }
 
   private handleError() {
--- src/modules/events/event.service.ts
+++ src/modules/events/event.service.ts
@@ -1,19 +1,26 @@
 import { Prisma } from "../../generated/prisma";
+import { ApiError } from "../../utils/api-error";
+import { generateSlug } from "../../utils/generate-slug";
+import { CloudinaryService } from "../cloudinary/cloudinary.service";
 import { PaginationQueryParams } from "../pagination/dto/pagination.dto";
 import { PrismaService } from "../prisma/prisma.service";
+import { CreateEventDTO } from "./dto/create-event.dto";
 import { GetEventsDTO } from "./dto/get-events.dto";
-
 
 export class EventService {
   private prisma: PrismaService;
+  private cloudinaryService: CloudinaryService;
   constructor() {
     this.prisma = new PrismaService();
+    this.cloudinaryService = new CloudinaryService();
   }
 
-  getBlogs = async (query: GetEventsDTO) => {
+  getEvents = async (query: GetEventsDTO) => {
     const { take, page, sortBy, sortOrder, search } = query;
 
-    const whereCluse: Prisma.EventWhereInput = {};
+    const whereCluse: Prisma.EventWhereInput = {
+      deletedAt: null,
+    };
 
     if (search) {
       whereCluse.title = { contains: search, mode: "insensitive" };
@@ -24,7 +31,6 @@ export class EventService {
       orderBy: { [sortBy]: sortOrder },
       skip: (page - 1) * take,
       take: take,
-      include: { admin: { omit: { password: true } } }, // join ke table user
     });
 
     const total = await this.prisma.event.count({ where: whereCluse });
@@ -33,5 +39,32 @@ export class EventService {
       data: events,
       meta: { page, take, total },
     };
+  };
+
+  createEvent = async (
+    body: CreateEventDTO,
+    thumbnail: Express.Multer.File,
+    autUserId: number
+  ) => {
+    const event = await this.prisma.event.findFirst({
+      where: { title: body.title },
+    });
+
+    if (event) {
+      throw new ApiError("title already in use", 400);
+    }
+
+    const slug = generateSlug(body.title);
+
+    const { secure_url } = await this.cloudinaryService.upload(thumbnail);
+
+    return await this.prisma.event.create({
+      data: {
+        ...body,
+        thumbnail: secure_url,
+        adminId: autUserId,
+        slug: slug,
+      },
+    });
   };
 }
