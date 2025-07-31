9c83cc0fa2e75a33adabd901b915384c28e8d6c8
DimPraditya <dimprad748296@gmail.com>
2025-07-31 12:40:53 +0700

    feat: add profile

--- src/modules/auth/auth.router.ts
+++ src/modules/auth/auth.router.ts
@@ -3,19 +3,25 @@ import { AuthController } from "./auth.controller";
 import { validateBody } from "../../middlewares/validation.middleware";
 import { RegisterDto } from "./dto/register.dto";
 import { LoginDto } from "./dto/login.dto";
+import { UploaderMiddleware } from "../../middlewares/uploader.middleware";
 
 export class AuthRouter {
   private authController: AuthController;
   private router: Router;
+  private uploaderMiddleware: UploaderMiddleware;
   constructor() {
     this.router = Router();
     this.authController = new AuthController();
+    this.uploaderMiddleware = new UploaderMiddleware();
     this.initializeRoutes();
   }
 
   private initializeRoutes = () => {
     this.router.post(
       "/register",
+      this.uploaderMiddleware
+        .upload()
+        .fields([{ name: "profilePicture", maxCount: 1 }]),
       validateBody(RegisterDto),
       this.authController.register
     );
