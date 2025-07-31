0e779d5fbf48856761c53966618da2695077efb7
DimPraditya <dimprad748296@gmail.com>
2025-07-31 13:36:50 +0700

    feat: update-profile (#8)

--- src/modules/profile/profile.controller.ts
+++ src/modules/profile/profile.controller.ts
@@ -8,14 +8,21 @@ export class ProfileController {
     this.profileService = new ProfileService();
   }
 
+  getProfile = async (req: Request, res: Response) => {
+    const authUserId = res.locals.user.id;
+    const result = await this.profileService.getProfile(authUserId);
+    res.status(200).send(result);
+  };
+
   updateProfile = async (req: Request, res: Response) => {
     const files = req.files as { [fieldname: string]: Express.Multer.File[] };
     const pictureProfile = files.pictureProfile?.[0];
-
     if (!pictureProfile) throw new ApiError("pictureProfile is required", 400);
-    
     const authUserId = res.locals.user.id;
+    const name = req.body.name;
+
     const result = await this.profileService.updateProfile(
+      name,
       pictureProfile,
       authUserId
     );
--- src/modules/profile/profile.router.ts
+++ src/modules/profile/profile.router.ts
@@ -2,6 +2,8 @@ import { Router } from "express";
 import { JwtMiddleware } from "../../middlewares/jwt.middleware";
 import { UploaderMiddleware } from "../../middlewares/uploader.middleware";
 import { ProfileController } from "./profile.controller";
+import { validateBody } from "../../middlewares/validation.middleware";
+import { UpdateProfileDto } from "./dto/update-profile.dto";
 
 export class ProfileRouter {
   private router: Router;
@@ -17,17 +19,23 @@ export class ProfileRouter {
   }
 
   private initializeRoutes = () => {
+    this.router.get(
+      "/",
+      this.jwtMiddleware.verifyToken(process.env.JWT_SECRET!),
+      this.profileController.getProfile
+    );
     this.router.patch(
       "/update-profile",
       this.jwtMiddleware.verifyToken(process.env.JWT_SECRET!),
       this.uploaderMiddleware
         .upload()
-        .fields([{ name: "thumbnail", maxCount: 1 }]),
+        .fields([{ name: "pictureProfile", maxCount: 1 }]),
       this.uploaderMiddleware.fileFilter([
         "image/jpeg",
         "image/png",
         "image/avif",
       ]),
+      validateBody(UpdateProfileDto),
       this.profileController.updateProfile
     );
 
